3c902bfe6d2728e55f8f85c66154ec0a64e7f391
Nacereddine_rbh <llbl2153@gmail.com>
2022-11-18 16:33:27 +0100

    commit test

--- Components/OurProducts.tsx
+++ Components/OurProducts.tsx
@@ -4,8 +4,8 @@ type Props = {};
 
 export default function OurProducts({}: Props) {
   return (
-    <div className="relative grid grid-rows-[1fr_3fr] w-screen">
-      <div className="row-start-1 flex flex-col gap-7">
+    <div className="relative grid grid-rows-[1fr_3fr] w-screen gap-9">
+      <div className="row-start-1 flex flex-col gap-6">
         <span className="font-bold text-[38px] md:text-[68px] lg:text-[55px] text-center truncate ">
           Our Featured Products
         </span>
@@ -15,7 +15,7 @@ export default function OurProducts({}: Props) {
           <br /> over the world to thousands of our partners
         </span>
       </div>
-      <div className="row-start-2">prods</div>
+      <div className="row-start-2"></div>
     </div>
   );
 }
