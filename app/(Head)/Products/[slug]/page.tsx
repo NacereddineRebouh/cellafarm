f4cc9dd0540e4fad333eb67ce07718094c6414bd
Nacereddine_rbh <llbl2153@gmail.com>
2022-12-15 17:54:51 +0100

    test2

--- app/(Head)/Products/[slug]/page.tsx
+++ app/(Head)/Products/[slug]/page.tsx
@@ -100,6 +100,7 @@ export default async function page({ params }: PostPageProps) {
           <div className="absolute top-[2%] left-[2%] cursor-default rounded-xl border-[2px] border-zinc-200 py-2 px-3 font-medium text-zinc-400">
             <p>ref_{product?.id ?? slug}</p>
             <p>ref_{product?.id ?? JSON.stringify(prod)}</p>
+            <p>ref_{product?.id ?? prod?.Id}</p>
           </div>
         </div>
         <div className="relative grid w-full flex-initial grid-rows-[30%_30%_40%] bg-[#74827E] p-7 lg:h-full lg:w-[40%]  xl:p-12 4xl:p-20">
