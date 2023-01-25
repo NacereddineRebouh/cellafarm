b0303f4697fba417fbc6528c93fa1e599e4eaf60
Nacereddine_rbh <llbl2153@gmail.com>
2023-01-25 14:29:45 +0100

    protected route

--- middleware.tsx
+++ middleware.tsx
@@ -1,8 +1,8 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/Profile', '/Shop', '/Products/:path*', '/Products'],
-  // matcher: ['/Products', '/Profile', '/Shop', '/Products/:path'],
+  // matcher: ['/Profile', '/Shop', '/Products/:path*', '/Products'],
+  matcher: ['/Products', '/Profile', '/Shop', '/Products/:path'],
 
   //   '/Products/:path*', '/Products'
 }
