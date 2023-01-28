d52ea2416ef7c532fa66206767ef622ed5a1632a
Nacereddine_rbh <llbl2153@gmail.com>
2023-01-28 16:48:10 +0100

    fix: og image

--- app/(Head)/head.tsx
+++ app/(Head)/head.tsx
@@ -17,8 +17,12 @@ export default function Head() {
         name="description"
         property="og:description"
         content="Cellafarm: We believe Future of Food is Here"
+      ></meta>
+      <meta
+        name="image"
+        property="og:image"
+        content="https://cellafarm.vercel.app/og/og_image.png"
       />
-      <meta property="og:image" content="/og/og_image.png" />
 
       {/* <!-- Twitter Meta Tags --> */}
       <meta name="twitter:card" content="summary_large_image" />
@@ -29,7 +33,10 @@ export default function Head() {
         name="twitter:description"
         content="Cellafarm: We believe Future of Food is Here"
       />
-      <meta name="twitter:image" content="/og/og_image.png" />
+      <meta
+        name="twitter:image"
+        content="https://cellafarm.vercel.app/og/og_image.png"
+      />
       {/* -------twitter end------- */}
 
       <meta
